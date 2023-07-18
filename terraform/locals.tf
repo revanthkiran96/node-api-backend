03bcecbb111cfed87ce2c020fc23e24f8a66ffc5
revanthkiran96 <116859708+revanthkiran96@users.noreply.github.com>
2023-07-18 20:09:33 +0530

    Update locals.tf

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -4,13 +4,13 @@ locals {
     Environment = "DEV"
     Terraform = "true"
   }
-  rds_secret_arn = data.aws_ssm_parameter.rds_secret_arn.value
+  rds_secret_arn = data.aws_ssm_parameter.rds_secret_arn.value 
   app_alb_security_group_id = data.aws_ssm_parameter.app_alb_security_group_id.value
   vpc_id = data.aws_ssm_parameter.vpc_id.value
-  rds_endpoint = split(":", data.aws_ssm_parameter.rds_endpoint.value)[0]
+  rds_endpoint = split(":",data.aws_ssm_parameter.rds_endpoint.value)[0]
   rds_security_group_id = data.aws_ssm_parameter.rds_security_group_id.value
-  ecs_cluster_id = data.aws_ssm_parameter.ecs_cluster_id.value
-  private_subent_ids = split(",",data.aws_ssm_parameter.private_subent_ids.value)
+  ecs_cluster_id = data.aws_ssm_parameter.ecs_cluster_id.value 
+  private_subnet_ids = split(",",data.aws_ssm_parameter.private_subnet_ids.value)
   app_target_group_arns = data.aws_ssm_parameter.app_target_group_arns.value
   container_name = "api-ecs"
   env_vars = [
@@ -36,7 +36,7 @@ locals {
     }
   ]
 
-  application_secrets = [
+ application_secrets = [
     {
         "name" : "DBPASS",
         "valueFrom" : "${local.rds_secret_arn}"
