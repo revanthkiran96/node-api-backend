6b2083b5318213c061ae4c70766cd574b7abf80f
revanthkiran96 <116859708+revanthkiran96@users.noreply.github.com>
2023-07-18 20:08:08 +0530

    Update data.tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -8,7 +8,6 @@ data "aws_iam_policy_document" "ecs_trust" {
       }
     }
 }
-
 data "aws_iam_policy_document" "rds_secret" {
     statement {
       sid = "AllowECSToAccessRDSSecret"
@@ -18,36 +17,28 @@ data "aws_iam_policy_document" "rds_secret" {
       resources = ["${local.rds_secret_arn}"]
     }
 }
-
 data "aws_ssm_parameter" "rds_secret_arn" {
   name = "/timing/rds/rds_secret_arn"
 }
-
 data "aws_ssm_parameter" "app_alb_security_group_id" {
   name = "/timing/vpc/app_alb_security_group_id"
 }
-
 data "aws_ssm_parameter" "vpc_id" {
   name = "/timing/vpc/vpc_id"
 }
-
 data "aws_ssm_parameter" "rds_endpoint" {
   name = "/timing/rds/rds_endpoint"
 }
-
 data "aws_ssm_parameter" "rds_security_group_id" {
   name = "/timing/vpc/rds_security_group_id"
 }
-
 data "aws_ssm_parameter" "ecs_cluster_id" {
   name = "/timing/ecs/ecs_cluster_id"
 }
-
-data "aws_ssm_parameter" "private_subent_ids" {
-  name = "/timing/vpc/private_subent_ids"
+data "aws_ssm_parameter" "private_subnet_ids" {
+  name = "/timing/vpc/private_subnet_ids"
 }
 
 data "aws_ssm_parameter" "app_target_group_arns" {
-  name = "/timing/ec2/app_target_group_arns"
+  name = "/timing/app_alb/app_target_group_arns"
 }
-
